5bc6840f545e8200411395c1b9f72fda20009e3c
kimo5532 <shotbyabel@gmail.com>
2018-08-05 18:21:33 -0700

    create and dismiss modal

--- src/pages/quote/quote.ts
+++ src/pages/quote/quote.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the QuotePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -14,12 +7,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'quote.html',
 })
 export class QuotePage {
+  constructor (private viewCtrl: ViewController) {}
+  onClose() {
+    this.viewCtrl.dismiss();
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotePage');
-  }
-
 }
