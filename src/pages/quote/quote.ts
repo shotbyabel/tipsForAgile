76be94940c2944c90436212320191f2683882f46
kimo5532 <shotbyabel@gmail.com>
2018-08-05 18:34:59 -0700

    data showing on modal and dismiss

--- src/pages/quote/quote.ts
+++ src/pages/quote/quote.ts
@@ -7,7 +7,21 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
   templateUrl: 'quote.html',
 })
 export class QuotePage {
-  constructor (private viewCtrl: ViewController) {}
+  //store the quote data here
+  person: string;
+  text: string;
+
+  constructor (private viewCtrl: ViewController,
+              private navParams: NavParams) {
+
+    }
+
+    ionViewDidLoad() {
+      this.person = this.navParams.get('person');
+      this.text = this.navParams.get('text')
+
+    }
+
   onClose() {
     this.viewCtrl.dismiss();
 
